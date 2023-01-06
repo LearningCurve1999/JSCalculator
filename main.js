476bb31693d81fe4d53f372c5bcd43ede7fc7de9
Respond <respondphil@gmail.com>
2023-01-06 09:07:20 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let currentOperand = document.getElementById('currentOperand');
+currentOperand.addEventListener(onclick, function(){
+    
+})
+let previousOperand = document.getElementById('previousOperand');
+let allClear = document.getElementById('allClear');
+let number = document.querySelectorAll('')
+
